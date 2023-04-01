164297af36b5ea9a29a511c77064c6f865153a0b
ahmed <ahmedraslan28@gmail.com>
2023-04-02 01:04:11 +0200

    install restframework

--- api/views.py
+++ api/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+
+@api_view()
+def hello_world(request):
+    return Response({"message": "Hello, world!"})
