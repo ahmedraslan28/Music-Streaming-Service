d78014a15e4e62f8a271e3c07f6d41e6540057d2
ahmed <ahmedraslan28@gmail.com>
2023-04-09 23:33:48 +0200

    add the track duration to the album duration if it belond to an album

--- api/signals.py
+++ api/signals.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from api.models import Artist, Track
+from api.models import Artist, Track, Album
 
 from mutagen.mp3 import MP3
 
@@ -19,8 +19,13 @@ def create_artist_for_new_user(sender, instance, **kwargs):
 
 
 @receiver(post_save, sender=Track)
-def add_duration_for_new_track(sender, created, instance, **kwargs):
+def add_duration_for_new_track_and_for_album(sender, created, instance, **kwargs):
     if created:
         audio = MP3(instance.audio_file.path)
         instance.duration = audio.info.length
         instance.save()
+        if instance.album is not None:
+            album = Album.objects.get(pk=instance.album_id)
+            album.song_count += 1
+            album.duration += instance.duration
+            album.save()
