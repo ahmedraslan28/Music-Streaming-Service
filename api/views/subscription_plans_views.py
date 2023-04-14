1ad8841cd1e874eee3c8f357cd485931408221e0
ahmed <ahmedraslan28@gmail.com>
2023-04-14 16:30:50 +0200

    add Subscription Plan permissions

--- api/views/subscription_plans_views.py
+++ api/views/subscription_plans_views.py
@@ -1,15 +1,18 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from ..models import SubscriptionPlan
 from ..serializers import SubscriptionPlanSerializer
+from ..permissions import *
 
 
 class SubscriptionPlanList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAdminUser | IsReadyOnlyRequest]
     serializer_class = SubscriptionPlanSerializer
     queryset = SubscriptionPlan.objects.all()
 
 
 class SubscriptionPlanDetails(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAdminUser | IsReadyOnlyRequest]
     serializer_class = SubscriptionPlanSerializer
     queryset = SubscriptionPlan.objects.all()
     lookup_field = 'id'
